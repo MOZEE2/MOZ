4a6487ba9a266e46824a875b68205212c6643abf
MOZEE2 <moiseeze4@gmail.com>
2025-01-19 07:29:16 +0000

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const downloadButton = document.querySelector('.download-button');
+    downloadButton.addEventListener('click', () => {
+        alert('Your download will start shortly!');
+    });
+});
